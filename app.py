5aa264ac3cb72f436a2ac55fb3a1f0229a4858b4
Fernando Martínez <45270620+ferdmartin@users.noreply.github.com>
2023-04-22 22:54:24 -0400

    changes

--- app.py
+++ app.py
@@ -114,11 +114,16 @@ def main():
             # if st.button("Output Explanation"):
             explainer = TextExplainer(sampler=MaskingTextSampler())
             explainer.fit(text, model.predict_proba)
-            
-            html = explainer.explain_prediction(target_names=["Human", "AI"])._repr_html_()
+
+            explainer.explain_prediction(target_names=["Human", "AI"])
+            #html = eli5.format_as_html()
             #st.write(html, unsafe_allow_html=True)
-            st.components.v1.html(html, width = 800, height = 2000, scrolling = True)
+            #st.components.v1.html(html, width = 800, height = 2000, scrolling = True)
             #st.markdown(html, unsafe_allow_html=True)
+                        
+            
+            
+
             
 if __name__ == "__main__":
     main()
